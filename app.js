42d815f03e0efc9ccebe18f4c43f5c519f07ba7f
mursitkoc <uyemursit@yandex.com>
2024-03-17 01:35:59 +0300

    chart yapıldı data güncellemeleri yapılacak koordinat hesaplamaları yapılacak

--- app.js
+++ app.js
@@ -1,37 +1,96 @@
-const getCSSVal = (e, v) => e.style.getPropertyValue(v);
-const mod = (n, m) => ((n % m) + m) % m; // Fix negative Modulo
-const PI = Math.PI;
-const TAU = PI * 2;
+// Get the canvas element
+const ctx = document.getElementById('myChart').getContext('2d');
+ctx.canvas.style.backgroundColor = 'black';
+// Create the chart data
 
-const radar = (elRadar) => {
 
-  const elBeam = elRadar.querySelector(".beam");
-  const elsDot = elRadar.querySelectorAll(".dot");
-
-  const update = () => {
-    const beamAngle = parseFloat(getComputedStyle(elBeam).getPropertyValue("rotate")) * PI / 180 || 0;
-
-    elsDot.forEach(elDot => {
-      const x = getCSSVal(elDot, "--x") - 0.5;
-      const y = getCSSVal(elDot, "--y") - 0.0;
-      const dotAngle = mod(Math.atan2(y, x), TAU);
-      const opacity = mod(dotAngle - beamAngle, TAU) / TAU;
-      elDot.style.opacity = opacity;
-    });
-
-    requestAnimationFrame(update);
-  };
-  
-  update();
+const data = {
+  datasets: [{
+    label: 'kuzey tarafı',
+    data: [{
+      x: -10,
+      y: 0,
+      r: 10
+    }, {
+      x: 0,
+      y: 20,
+      r: 5
+    }, {
+      x: 10,
+      y: -10,
+      r: 15
+    }, {
+      x: -5,
+      y: -5,
+      r: 100
+    }, {
+      x: 100,
+      y: 0,
+      r: 100
+    }],
+    backgroundColor: 'rgba(255, 99, 132, 0.2)',
+    borderColor: 'rgba(255, 99, 132, 1)',
+    borderWidth: 1
+  },
+  {
+    label:'mihenk',
+    data:[{x:0,y:0,r:0}],
+    backgroundColor: 'rgba(255, 99, 132, 0.2)',
+    borderColor: 'rgba(255, 99, 132, 1)',
+    borderWidth: 9
+  }
+]
 };
 
-document.querySelectorAll(".radar").forEach(radar);
+// Create the chart
+const myChart = new Chart(ctx, {
+  type: 'scatter',
+  data: data,
+  options: {
+    scales: {
+      x: {
+        grid: {
+          color: 'rgba(0, 230, 0, 0.4)'
+        },
+        beginAtZero: true,
+        suggestedMin: -100,
+        suggestedMax: 100,
+        position:'center'
+      },
+      y: {
+        grid: {
+          color: 'rgba(0, 230, 0, 0.4)'
+        },
+        beginAtZero: true,
+       suggestedMin: -100,
+        suggestedMax: 100,
+        position:'center'
+      }
+    },
+    plugins: {
+      title: {
+        display: false,
+        text: 'Axis Center Positioning'
+      },
+      defaults: {
+        color: 'lightGreen'
+      },
+ 
+    }
+    // plugins: {
+    //   datalabels: {
+    //     formatter: (value, context) => {
+    //       const x = context.chart.scales.x.getValueForPixel(context.raw.x);
+    //       const y = context.chart.scales.y.getValueForPixel(context.raw.y);
+    //       const quadrant = Math.round((Math.atan2(y, x) / Math.PI) * 2) + 1;
+    //       return `Q${quadrant}: ${value}`;
+    //     },
+    //     color: 'black'
+    //   }
+    // }
+  }
+});
 
-const canvas = document.getElementById("myCanvas");
-const ctx = canvas.getContext("2d");
-
-ctx.moveTo(0, 0);
-ctx.lineTo(10, 100);
-ctx.lineWidth = 1;
-ctx.strokeStyle = "red";
-ctx.stroke();
+// myChart.options.scales.x.position = 'center';
+// myChart.options.scales.y.position = 'center';
+// myChart.update();
